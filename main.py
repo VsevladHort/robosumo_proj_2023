892a19e5eb69fb425c82b0674c77eadec9fff8cc
Vsevolod <vsevolod180203@gmail.com>
2023-11-13 12:03:33 +0000

    set motor pin factory explicitly

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import sys
 from time import sleep
 
-from gpiozero import Button, RGBLED, Motor, DistanceSensor
+from gpiozero import Button, RGBLED, Motor
 import program
 import edge_detection
 import ultrasonic_opponent_detection
 import numpy
 import vl53l5cx_ctypes as vl53l5cx
 
-DISTANCE_THRESHOLD = 700 # 70cm limit in mm
-DISTANCE_THRESHOLD_ULTRASONIC = 0.7 # 70cm limit
-TURNING_SPEED_FOR_SEARCHING_TARGET = 0.5 # speed at which to run the motors while turning during search
-TURNING_SPEED_FOR_CENTERING_TARGET = 0.6 # Scales the speed at which robot turns when trying to keep it's target in the center of it's FOV
+from gpiozero.pins.rpigpio import RPiGPIOFactory
+
+factory = RPiGPIOFactory()
+
+DISTANCE_THRESHOLD = 700  # 70cm limit in mm
+DISTANCE_THRESHOLD_ULTRASONIC = 0.7  # 70cm limit
+TURNING_SPEED_FOR_SEARCHING_TARGET = (
+    0.5  # speed at which to run the motors while turning during search
+)
+TURNING_SPEED_FOR_CENTERING_TARGET = 0.6  # Scales the speed at which robot turns when trying to keep it's target in the center of it's FOV
 
 print("Uploading firmware, please wait...")
 vl53 = vl53l5cx.VL53L5CX()
@@ -22,14 +28,16 @@ this_program = program.ProgramStatus()
 button_start = Button(17)  # these are BCM numbers
 button_stop = Button(27)
 
-motor_left = Motor(5, 6)
-motor_right = Motor(13, 19)
+motor_left = Motor(5, 6, pin_factory=factory)
+motor_right = Motor(13, 19, pin_factory=factory)
 
 edge_detector = edge_detection.EdgeDetector()
 
-ultrasonic_opponent_detector = ultrasonic_opponent_detection.OpponentDetector(DISTANCE_THRESHOLD_ULTRASONIC)
+ultrasonic_opponent_detector = ultrasonic_opponent_detection.OpponentDetector(
+    DISTANCE_THRESHOLD_ULTRASONIC
+)
 
-last_seen = -1 # the last direction opponent was found
+last_seen = -1  # the last direction opponent was found
 # -1 - not found
 # 0 - front
 # 1 - left
@@ -99,15 +107,15 @@ def handle_edge_detection():
         return True
     else:
         return False
-    
 
-def consider_ultrasonic_sensors ():
+
+def consider_ultrasonic_sensors():
     distances = ultrasonic_opponent_detector.get_sensor_states()
-    if (distances["left"] > 0 and distances["right"] > 0):
-        if distances["left"] < distances["right"]: 
+    if distances["left"] > 0 and distances["right"] > 0:
+        if distances["left"] < distances["right"]:
             turn_left()
             return 1
-        else: 
+        else:
             turn_right()
             return 2
     elif distances["left"] > 0:
@@ -118,7 +126,6 @@ def consider_ultrasonic_sensors ():
         return 2
     else:
         return -1
-    
 
 
 def handle_opponent_search():
@@ -154,7 +161,9 @@ def handle_opponent_search():
         else:
             target_distance = 0
 
-        distance = numpy.flip(distance, axis=0) # I am having a hard time visualising this, should we really be flipping the matrix?
+        distance = numpy.flip(
+            distance, axis=0
+        )  # I am having a hard time visualising this, should we really be flipping the matrix?
         # Not so sure this will work, we'll have to tinker quite a bit here, I expect.
 
         # Calculate the center of mass along X and Y (Should probably just remove Y in the future)
@@ -171,19 +180,23 @@ def handle_opponent_search():
             for oy in range(8):
                 for ox in range(8):
                     x += distance[ox][oy] * oy
-            x /= scalar # x should be the only thing we care about when it comes to aligning the robot
-            x /= 3.5 # 3.5 - average value of our coordinates
-            x -= 1.0 # at this point x is in range from 0 to 2, we bring it to -1 to 1 for speed control
+            x /= scalar  # x should be the only thing we care about when it comes to aligning the robot
+            x /= 3.5  # 3.5 - average value of our coordinates
+            x -= 1.0  # at this point x is in range from 0 to 2, we bring it to -1 to 1 for speed control
 
             print("Object detected at x: {:.2f}, y: {:.2f}".format(x, y))
 
             # Our robot will try to attack the target at full speed.
             print("Distance is {:.1f} mm.".format(target_distance))
 
-            motor_left.forward(min(1 - (x * TURNING_SPEED_FOR_CENTERING_TARGET)), 1) # x > 0 will make the robot start turning right
-            motor_right.forward(min(1 + (x * TURNING_SPEED_FOR_CENTERING_TARGET), 1)) # x < 0 will make the robot start turning left
-            return 0 # Let's consider 0 to be an indicator that opponent is in front of the robot
-        else: 
+            motor_left.forward(
+                min(1 - (x * TURNING_SPEED_FOR_CENTERING_TARGET)), 1
+            )  # x > 0 will make the robot start turning right
+            motor_right.forward(
+                min(1 + (x * TURNING_SPEED_FOR_CENTERING_TARGET), 1)
+            )  # x < 0 will make the robot start turning left
+            return 0  # Let's consider 0 to be an indicator that opponent is in front of the robot
+        else:
             return consider_ultrasonic_sensors()
     else:
         return consider_ultrasonic_sensors()
@@ -200,7 +213,7 @@ if __name__ == "__main__":
                 if first_launch:
                     sleep(5)  # sleep in the main thread
                     first_launch = False
-                led.color = (0, 1, 0) # light up the LED green
+                led.color = (0, 1, 0)  # light up the LED green
                 if handle_edge_detection():
                     sleep(1)  # give the robot some time to get away from the edge
                 opponent_search_result = handle_opponent_search()
@@ -212,7 +225,7 @@ if __name__ == "__main__":
                     turn_right()
                 elif last_seen != 0:
                     turn_right()
-                sleep(0.01) # Do we need it?
+                sleep(0.01)  # Do we need it?
             else:
                 first_launch = True
                 motor_left.stop()
