34fb1b057ce80465c3c9e482deffd88beb6072f2
Vsevolod <vsevolod180203@gmail.com>
2023-12-03 19:09:58 +0000

    add ultrasonic_test script

---
+++ ultrasonic_test.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import ultrasonic_opponent_detection
+from time import sleep
+
+detector = ultrasonic_opponent_detection.OpponentDetector(70)
+try:
+    while True:
+        print(detector.get_sensor_states())
+        sleep(1)
+except KeyboardInterrupt as e:
+    print(e)
+finally:
+    GPIO.cleanup()
